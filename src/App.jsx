8019e3192dcb315d159f59725d5c2e54ed7b03ed
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2022-12-25 19:01:20 +0530

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from './Components/Header/Header';
-import Home from './Pages/Home/Home';
-import MovieList from './Components/MovieList/MovieList';
-import Movie from './Pages/MovieDetails/Movie';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home/Home";
+import MovieList from "./Components/MovieList/movieList";
+import Movie from "./Pages/MovieDetails/Movie";
+import "./App.css";
 import PageNotFound from "./Pages/Page 404/Page404";
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Header />
-            <Routes>
-                <Route index element={<Home />}></Route>
-                <Route path="movie/:id" element={<Movie />}></Route>
-                <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<PageNotFound/>}></Route>
-            </Routes>
-        </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<PageNotFound />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
-import MovieList from "../../components/movieList/movieList";
+import MovieList from "../../Components/MovieList/movieList";
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -26,37 +26,38 @@ const Home = () => {
           infiniteLoop={true}
           showStatus={false}
         >
-          {React.Children.toArray( 
-          popularMovies.map((movie) => (
-            <Link
-              key={movie.id}
-              style={{ textDecoration: "none", color: "white" }}
-              to={`/movie/${movie.id}`}
-            >
-              <div className="posterImage">
-                <img
-                  src={`https://image.tmdb.org/t/p/original${
-                    movie && movie.backdrop_path
-                  }`}
-                />
-              </div>
-              <div className="posterImage__overlay">
-                <div className="posterImage__title">
-                  {movie ? movie.original_title : ""}
+          {React.Children.toArray(
+            popularMovies.map((movie) => (
+              <Link
+                key={movie.id}
+                style={{ textDecoration: "none", color: "white" }}
+                to={`/movie/${movie.id}`}
+              >
+                <div className="posterImage">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${
+                      movie && movie.backdrop_path
+                    }`}
+                  />
                 </div>
-                <div className="posterImage__runtime">
-                  {movie ? movie.release_date : ""}
-                  <span className="posterImage__rating">
-                    {movie ? movie.vote_average : ""}
-                    <i className="fas fa-star" />{" "}
-                  </span>
+                <div className="posterImage__overlay">
+                  <div className="posterImage__title">
+                    {movie ? movie.original_title : ""}
+                  </div>
+                  <div className="posterImage__runtime">
+                    {movie ? movie.release_date : ""}
+                    <span className="posterImage__rating">
+                      {movie ? movie.vote_average : ""}
+                      <i className="fas fa-star" />{" "}
+                    </span>
+                  </div>
+                  <div className="posterImage__description">
+                    {movie ? movie.overview : ""}
+                  </div>
                 </div>
-                <div className="posterImage__description">
-                  {movie ? movie.overview : ""}
-                </div>
-              </div>
-            </Link>
-          )))}
+              </Link>
+            ))
+          )}
         </Carousel>
         <MovieList />
       </div>
